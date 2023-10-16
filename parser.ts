20844711e2a739adccfaad52ffe2080482184689
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-10-15 20:21:02 -0600

    Lots of work on parser

--- parser.ts
+++ parser.ts
@@ -16,6 +16,19 @@ import {
 export default class Parser {
     private tokens: Token[] = [];
 
+    private not_eof (): boolean {
+        return this.tokens[0].type != TokenType.EOF;
+    }
+
+    private at () {
+        return this.tokens[0] as Token;
+    }
+
+    private eat () {
+        const prev = this.tokens.shift() as Token;
+        return prev;
+    }
+
     public produceAST(sourceCode: string): Program {
 
         this.tokens = tokenize(sourceCode);
@@ -23,8 +36,38 @@ export default class Parser {
         const program: Program = {
             kind: "Program",
             body: [],
+        };
+
+        // parse until end of file
+        while(this.not_eof()) {
+            program.body.push(this.parse_stmt());
         }
 
         return program;
+    }
+
+    private parse_stmt (): Stmt {
+        // skip to parse_expr
+        return this.parse_expr();
+    }
+
+    private parse_expr (): Expr {
+        return this.parse_primary_expr();
+    }
+
+    private parse_primary_expr (): Expr {
+        const tk = this.at().type;
+
+        switch (tk) {
+            case TokenType.Identifier:
+                return { kind: "Identifier", symbol: this.eat().value } as Identifier;
+
+            case TokenType.Number:
+                return { kind: "NumericLiteral", value: parseFloat(this.eat().value) } as NumericLiteral;
+
+            default:
+                console.error("Unexpected token found during parsing!", this.at());
+                Deno.exit(1);
+        }
     }
 }
