b2dcab16e4121fdc316e4f159e345c41ed7c8722
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-10-15 20:00:59 -0600

    Added more types

--- ast.ts
+++ ast.ts
@@ -1,3 +1,4 @@
+// deno-lint-ignore-file no-empty-interface
 export type NodeType = 
     | "Program"
     | "NumericLiteral"
@@ -11,4 +12,23 @@ export interface Stmt {
 export interface Program extends Stmt {
     kind: "Program",
     body: Stmt[];
+}
+
+export interface Expr extends Stmt {}
+
+export interface BinaryExpr extends Expr {
+    kind: "BinaryExpr";
+    left: Expr;
+    right: Expr;
+    operator: string;
+}
+
+export interface Identifier extends Expr {
+    kind: "Identifier";
+    symbol: string;
+}
+
+export interface NumericLiteral extends Expr {
+    kind: "NumericLiteral";
+    value: number;
 }
