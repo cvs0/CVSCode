39677dabfb5a82e6ae22f40cb4a7fa60a4735d7f
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-10-15 19:35:04 -0600

    Some tokenizer work

--- lexer.ts
+++ lexer.ts
@@ -6,7 +6,11 @@ export enum TokenType {
     OpenParen,
     CloseParen,
     BinaryOperator,
-    Let
+    Let,
+}
+
+const KEYWORDS: Record<string, TokenType> = {
+    "let": TokenType.Let,
 }
 
 export interface Token {
@@ -18,6 +22,17 @@ function token (value = "", type: TokenType): Token {
     return { value, type};
 }
 
+function isAlpha (src: string) {
+    return src.toUpperCase() != src.toLowerCase();
+}
+
+function isInt (str: string) {
+    const c = str.charCodeAt(0);
+    const bounds = ['0'.charCodeAt(0), '9'.charCodeAt(0)];
+
+    return (c >= bounds[0] && c <= bounds[1]);
+}
+
 export function tokenize (sourceCode: string): Token[] {
     const tokens = new Array<Token>();
     const src = sourceCode.split("")
@@ -26,6 +41,33 @@ export function tokenize (sourceCode: string): Token[] {
     while (src.length > 0) {
         if(src[0] == '(') {
             tokens.push(token(src.shift(), TokenType.OpenParen));
+        } else if (src[0] == ")") {
+            tokens.push(token(src.shift(), TokenType.CloseParen));
+        } else if (src[0] == "+" || src[0] == "-" || src[0] == "*" || src[0] == "/") {
+            tokens.push(token(src.shift(), TokenType.BinaryOperator));
+        } else if (src[0] == '=') {
+            tokens.push(token(src.shift(), TokenType.Equals));
+        } else {
+            // Handles multicharacter tokens
+
+            // Build number token
+            if(isInt(src[0])) {
+                let num = "";
+
+                while (src.length > 0 && isInt(src[0])) {
+                    num += src.shift();
+                }
+
+                tokens.push(token(num, TokenType.ident));
+            } else if (isAlpha(src[0])) {
+                let ident = "";
+
+                while (src.length > 0 && isAlpha(src[0])) {
+                    ident += src.shift();
+                }
+
+                tokens.push(token(ident, TokenType.Identifier));
+            }
         }
     }
 
