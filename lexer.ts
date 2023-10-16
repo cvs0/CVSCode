24172a896168f402f2930eb9e38802820bf1a9e2
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-10-15 19:44:07 -0600

    Added Deno and some skippable chars

--- lexer.ts
+++ lexer.ts
@@ -26,6 +26,10 @@ function isAlpha (src: string) {
     return src.toUpperCase() != src.toLowerCase();
 }
 
+function isSkippable (str: string) {
+    return str == ' ' || str == '\n' || str == '\t';
+}
+
 function isInt (str: string) {
     const c = str.charCodeAt(0);
     const bounds = ['0'.charCodeAt(0), '9'.charCodeAt(0)];
@@ -58,7 +62,7 @@ export function tokenize (sourceCode: string): Token[] {
                     num += src.shift();
                 }
 
-                tokens.push(token(num, TokenType.ident));
+                tokens.push(token(num, TokenType.Identifier));
             } else if (isAlpha(src[0])) {
                 let ident = "";
 
@@ -68,12 +72,17 @@ export function tokenize (sourceCode: string): Token[] {
 
                 // check for reserved keywords
                 const reserved = KEYWORDS[ident];
-                
+
                 if(reserved == undefined) {
                     tokens.push(token(ident, TokenType.Identifier));
                 } else {
                     tokens.push(token(ident, reserved));
                 }
+            } else if (isSkippable(src[0])) {
+                src.shift();
+            } else {
+                console.log("Unrecognized character found in source: ", src[0]);
+                Deno.exit();
             }
         }
     }
