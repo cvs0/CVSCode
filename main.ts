f96ffefad24149e1e85f2da4176f9f39e6ac201f
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-10-16 18:01:57 -0600

    Fixes + actual interpretation

--- main.ts
+++ main.ts
@@ -1,26 +1,23 @@
 import Parser from "./frontend/parser.ts";
 import { evaluate } from "./runtime/interpreter.ts";
-
 repl();
 
-function repl () {
-    const parser = new Parser();
+function repl() {
+  const parser = new Parser();
+  console.log("\nRepl v0.1");
 
-    console.log("\nRepl v0.1");
-
-    while (true) {
-        const input = prompt("> ");
-
-        if(!input || input.includes("exit")) {
-            Deno.exit(1);
-        }
-
-        const program = parser.produceAST(input);
-        console.log(program);
-
-        const result = evaluate(program);
-        console.log(result);
-
-        console.log("------\n\n");
+  // Continue Repl Until User Stops Or Types `exit`
+  while (true) {
+    const input = prompt("> ");
+    // Check for no user input or exit keyword.
+    if (!input || input.includes("exit")) {
+      Deno.exit(1);
     }
+
+    // Produce AST From source-code
+    const program = parser.produceAST(input);
+
+    const result = evaluate(program);
+    console.log(result);
+  }
 }
--- runtime/interpreter.ts
+++ runtime/interpreter.ts
@@ -1,5 +1,45 @@
 import { ValueTypes, RuntimeVal, NumberVal, NullVal } from "./values.ts"
-import { NodeType, NumericLiteral, Stmt } from "../frontend/ast.ts"
+import { BinaryExpr, NodeType, NumericLiteral, Program, Stmt } from "../frontend/ast.ts"
+
+function eval_program(program: Program): RuntimeVal {
+    let lastEvaluated: RuntimeVal = { type: "null", value: "null"} as NullVal;
+
+    for (const statement of program.body) {
+        lastEvaluated = evaluate(statement);
+    }
+
+    return lastEvaluated;
+}
+
+function eval_numeric_binary_expr (lhs: NumberVal, rhs: NumberVal, operator: string): NumberVal {
+    let result: number;
+
+    if(operator == "+") {
+        result = lhs.value + rhs.value;
+    } else if (operator == "-") {
+        result = lhs.value - rhs.value;
+    } else if (operator == "*") {
+        result = lhs.value * rhs.value;
+    } else if (operator == "/") {
+        // TODO: division by zero checks
+        result = lhs.value / rhs.value;
+    } else {
+        result = lhs.value % rhs.value;
+    }
+
+    return { value: result, type: "number" }
+}
+
+function eval_binary_expr (binop: BinaryExpr): RuntimeVal {
+    const lhs = evaluate(binop.left);
+    const rhs = evaluate(binop.right);
+
+    if (lhs.type == "number" && rhs.type == "number") {
+        return eval_numeric_binary_expr(lhs as NumberVal, rhs as NumberVal, binop.operator);
+    }
+
+    return { type: "null", value: "null" } as NullVal;
+}
 
 export function evaluate (astNode: Stmt): RuntimeVal {
 
@@ -15,6 +55,12 @@ export function evaluate (astNode: Stmt): RuntimeVal {
                 value: "null",
                 type: "null"
             } as NullVal;
+        
+        case  "BinaryExpr":
+            return eval_binary_expr(astNode as BinaryExpr);
+        
+        case "Program":
+            return eval_program(astNode as Program);
 
         default:
             console.error("This AST Node has not yet been setup for interpretation.", astNode);
