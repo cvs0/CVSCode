af9ffee10c7a3cd50f2722be9c83ab00a8d3d975
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-10-16 18:10:57 -0600

    Added variable assigning / scoping

--- runtime/environment.ts
+++ runtime/environment.ts
@@ -11,4 +11,34 @@ export default class Environment {
         this.parent = parentENV;
         this.variables = new Map();
     }
+
+    public declareVar (varname: string, value: RuntimeVal): RuntimeVal {
+        if (this.variables.has(varname)) {
+            throw `Cannot declare variable ${varname}. As it already is defined.`;
+        }
+
+        this.variables.set(varname, value);
+
+        return value;
+    }
+
+    public assignVar (varname: string, value: RuntimeVal): RuntimeVal {
+        const env = this.resolve(varname);
+        
+        env.variables.set(varname, value);
+
+        return value;
+    }
+
+    public resolve (varname: string): Environment {
+        if (this.variables.has(varname)) {
+            return this;
+        }
+
+        if(this.parent == undefined) {
+            throw `Cannot resolve '${varname}' as it does not exist.`;
+        }
+
+        return this.parent.resolve(varname);
+    }
 }
