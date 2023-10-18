871a3245c0d804eef07bc5c49e03eb275ed19ecd
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-10-18 17:59:52 -0600

    Updated Repl to 0.2 and allowed files to be ran

--- main.ts
+++ main.ts
@@ -4,13 +4,22 @@ import { evaluate } from "./runtime/interpreter.ts";
 import { MK_NULL, MK_BOOL, MK_NUMBER, NumberVal } from "./runtime/values.ts";
 repl();
 
+run("./test.txt");
+
+async function run (filename: string) {
+  const parser = new Parser();
+  const env = new Environment();
+  
+  const input = await Deno.readTextFile(filename);
+  const program = parser.produceAST(input);
+  const result = evaluate(program, env);
+  console.log(result);
+}
+
 function repl() {
   const parser = new Parser();
   const env = new Environment();
-  env.declareVar("true", MK_BOOL(true), true);
-  env.declareVar("false", MK_BOOL(false), true);
-  env.declareVar("null", MK_NULL(), true);
-  console.log("\nRepl v0.1");
+  console.log("\nRepl v0.2");
 
   // Continue Repl Until User Stops Or Types `exit`
   while (true) {
