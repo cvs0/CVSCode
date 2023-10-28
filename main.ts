19556868e0741b427076823f70f99cd08b0abfb6
cvs0 <143862758+cvs0@users.noreply.github.com>
2023-10-28 10:20:06 -0600

    cleanup

--- main.ts
+++ main.ts
@@ -7,7 +7,6 @@ async function run(filename: string) {
   const env = createGlobalEnv();
 
   try {
-    // Ensure that the file type ends with ".csv"
     if (!filename.toLowerCase().endsWith(".cvs")) {
       console.error("Invalid file type. You must provide a CVSCode file (.cvs).");
       return;
@@ -17,7 +16,6 @@ async function run(filename: string) {
     const program = parser.produceAST(input);
 
     const result = evaluate(program, env);
-    // console.log(result);
   } catch (error) {
     console.error(`Error reading or evaluating file: ${error.message}`);
   }
@@ -28,11 +26,9 @@ async function repl() {
   const env = createGlobalEnv();
   console.log("\nRepl v0.3");
 
-  // Continue Repl Until User Stops Or Types `exit`
   while (true) {
     const input = prompt("> ");
 
-    // Check for no user input or exit keyword.
     if (!input || input.includes("exit")) {
       Deno.exit(1);
     }
@@ -44,32 +40,27 @@ async function repl() {
       continue;
     }
 
-    // Check if the input contains the "run" keyword.
     if (input.includes("run")) {
 
-      // Extract the filename from the input.
       const fileNameMatch = input.match(/run\s+(\S+)/);
       if (fileNameMatch) {
 
         const fileName = fileNameMatch[1];
 
-        await run(fileName); // Use await here to wait for the run function to complete.
+        await run(fileName);
 
-        continue; // Skip the evaluation and prompt for the next input.
+        continue;
       } else {
         console.error("Invalid run command. Use 'run <filename>.cvs'.");
 
-        continue; // Prompt for the next input.
+        continue;
       }
     }
 
-    // Produce AST From source code
     const program = parser.produceAST(input);
 
-    // evaluate the program
     const result = evaluate(program, env);
 
-    // log the evaluated result
     console.log(result);
   }
 }
